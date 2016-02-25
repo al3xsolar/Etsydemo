c75862392f15e8dece69045b3bead5072a9c026b
al3xsolar <al3xsolar@gmail.com>
2016-02-25 17:39:00 -0600

    Test for thumb

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,8 @@
 class Listing < ActiveRecord::Base
 	if Rails.env.development?
-		has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "missing.png"
+		has_attached_file :image, styles: { medium: "200x", thumb: "100x100" }, default_url: "missing.png"
 	else
-		has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "missing.png",
+		has_attached_file :image, styles: { medium: "200x", thumb: "100x100" }, default_url: "missing.png",
 	 										:storage => :dropbox,
 	 										:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
 											:path => ":stle/id_:filename"
